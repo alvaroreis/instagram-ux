990d4cac4d553247bc8d9c3a5838aa13b17afb40
Alvaro Barros <alvaroluiz.rb@gmail.com>
2022-05-30 15:08:47 -0300

    add attr comments (int mock)

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,9 +1,11 @@
 class UserModel {
   String username;
   String imageUrl;
+  int comments;
   UserModel({
     required this.username,
     required this.imageUrl,
+    required this.comments,
   });
   bool get loggedIn => 'reisalvaro' == username;
 }
