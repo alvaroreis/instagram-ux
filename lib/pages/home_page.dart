c5a5eaeff4c3695892a4358092810bbdf21b2a35
Alvaro Barros <alvaroluiz.rb@gmail.com>
2022-05-30 15:09:42 -0300

    add bottomNavigationBar / generate int comments (mock)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
 import 'package:fluttericon/font_awesome_icons.dart';
+import 'package:iconly/iconly.dart';
 
 import '/models/user_model.dart';
 import '/pages/widgets/story_widget.dart';
@@ -17,36 +20,43 @@ class HomePage extends StatelessWidget {
     List<UserModel> users = [
       UserModel(
         username: 'reisalvaro',
+        comments: Random().nextInt(15),
         imageUrl:
             'https://tm.ibxk.com.br/2015/09/09/09165955100389.jpg?ims=1200x675',
       ),
       UserModel(
         username: 'robotzu',
+        comments: Random().nextInt(15),
         imageUrl:
             'https://img.elo7.com.br/product/original/3B6A5D4/edicao-de-fotos-reais-animes-ou-desenhos-edicao-de-desenho.jpg',
       ),
       UserModel(
         username: 'pintcloud',
+        comments: Random().nextInt(15),
         imageUrl:
             'https://ovicio.com.br/wp-content/uploads/2022/03/20220320-ovicio-one-piece-anime-luffy.jpg',
       ),
       UserModel(
         username: 'bronzefriend',
+        comments: Random().nextInt(15),
         imageUrl:
             'https://files.tecnoblog.net/wp-content/uploads/2022/03/dororo.jpg',
       ),
       UserModel(
         username: 'ettor',
+        comments: Random().nextInt(15),
         imageUrl:
             'https://metagalaxia.com.br/wp-content/uploads/2021/04/capa-animes-esporte-1024x576.jpg',
       ),
       UserModel(
         username: 'tribobuilder',
+        comments: Random().nextInt(15),
         imageUrl:
             'https://i0.wp.com/gamelogia.com.br/wp-content/uploads/2017/08/imagens-anime-33.jpg?resize=1280%2C640&ssl=1',
       ),
       UserModel(
         username: 'flipfrigate',
+        comments: Random().nextInt(15),
         imageUrl:
             'https://criticalhits.com.br/wp-content/uploads/2021/12/Satoru_Gojo_Anime-910x478.jpg',
       ),
@@ -73,6 +83,38 @@ class HomePage extends StatelessWidget {
       ),
     ];
 
+    var bottomNavigationBarItens = [
+      const BottomNavigationBarItem(
+        label: 'Home',
+        activeIcon: Icon(IconlyBold.home),
+        icon: Icon(IconlyLight.home),
+      ),
+      const BottomNavigationBarItem(
+        label: 'Search',
+        activeIcon: Icon(PhosphorIcons.magnifying_glass_bold),
+        icon: Icon(PhosphorIcons.magnifying_glass),
+      ),
+      const BottomNavigationBarItem(
+        label: '',
+        activeIcon: Icon(IconlyBold.activity),
+        icon: Icon(IconlyLight.activity),
+      ),
+      const BottomNavigationBarItem(
+        label: '',
+        activeIcon: Icon(IconlyBold.bag_2),
+        icon: Icon(IconlyLight.bag_2),
+      ),
+      BottomNavigationBarItem(
+        label: 'Profile',
+        icon: SizedBox(
+          height: 30,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(users.first.imageUrl),
+          ),
+        ),
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: theme.scaffoldBackgroundColor,
@@ -86,11 +128,18 @@ class HomePage extends StatelessWidget {
         ),
         actions: actions,
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 3,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: bottomNavigationBarItens,
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           SizedBox(
-            height: 120,
+            height: 100,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
               shrinkWrap: true,
@@ -100,9 +149,6 @@ class HomePage extends StatelessWidget {
                 isStoryProfile: users[index].loggedIn,
               ),
             ),
-          ),
-          const SizedBox(
-            height: 10,
           ),
           Expanded(
             child: ListView.builder(
